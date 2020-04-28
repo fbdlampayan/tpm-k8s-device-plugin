72a8f1f00a498b38154f4cb434d475e638112a9e
lampayan <francis.lampayan@nokia.com>
2020-04-28 16:10:48 +0200

    tpm inclusion with tpmrm

--- main.go
+++ main.go
@@ -12,17 +12,20 @@ import (
 
 const (
     devfsDriDirectory  = "/dev/tpmrm0"
+    devTpmFsDirectory = "/dev/tpm"
 
     namespace = "color.example.com"
 )
 
 type devicePlugin struct {
 	devfsDir string
+    tpmFsDir string
 }
 
-func newDevicePlugin(devfsDir string) *devicePlugin {
+func newDevicePlugin(devfsDir string, tpmFsDir string) *devicePlugin {
 	return &devicePlugin{
 		devfsDir:         devfsDir,
+        tpmFsDir:         tpmFsDir,
 	}
 }
 
@@ -53,21 +56,43 @@ func (dp *devicePlugin) scan() (dpapi.DeviceTree, error) {
         fmt.Printf("file %s stat error\n: %v", dp.devfsDir, err)
         return nil, errors.Wrap(err, "Permission denied /dev/mem")
     }
+
+    _, er := os.Stat(dp.tpmFsDir)
+    if er == nil {
+        fmt.Printf("file %s exists\n", dp.tpmFsDir)
+    } else if os.IsNotExist(err) {
+        fmt.Printf("file %s not exists\n", dp.tpmFsDir)
+        return nil, errors.Wrap(er, "Can't read /dev/tpm")
+    } else {
+        fmt.Printf("file %s stat error\n: %v", dp.tpmFsDir, er)
+        return nil, errors.Wrap(err, "Permission denied /dev/mem")
+    }
     
     devTree := dpapi.NewDeviceTree()
     
     var nodes []pluginapi.DeviceSpec
+    
     nodes = append(nodes, pluginapi.DeviceSpec{
         HostPath: dp.devfsDir,
         ContainerPath: dp.devfsDir,
         Permissions:   "rw",
     })
 
+    var tpmNodes  []pluginapi.DeviceSpec
+
+    tpmNodes = append(tpmNodes, pluginapi.DeviceSpec{
+        HostPath: dp.tpmFsDir,
+        ContainerPath: dp.tpmFsDir,
+        Permissions:   "rw",
+    })
+
+
     devID := "id1";
-    
     fmt.Printf("adding %s with id %s", dp.devfsDir, devID)
     devTree.AddDevice("yellow", devID, dpapi.NewDeviceInfo(pluginapi.Healthy, nodes, nil, nil))
-    devTree.AddDevice("red", "id2", dpapi.NewDeviceInfo(pluginapi.Healthy, nodes, nil, nil))
+
+    fmt.Print("adding %s with id %s", dp.tpmFsDir, devID)
+    devTree.AddDevice("red", "id2", dpapi.NewDeviceInfo(pluginapi.Healthy, tpmNodes, nil, nil))
 
     return devTree, nil
 }
@@ -76,7 +101,7 @@ func (dp *devicePlugin) scan() (dpapi.DeviceTree, error) {
 func main() {
     fmt.Println("my device plugin started")
 
-    plugin := newDevicePlugin(devfsDriDirectory)
+    plugin := newDevicePlugin(devfsDriDirectory, devTpmFsDirectory)
     manager := dpapi.NewManager(namespace, plugin)
     manager.Run()
 }
