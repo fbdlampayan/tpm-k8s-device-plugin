c80d69110355e8fa5a660dd95670c9d3c0ec2b39
lampayan <francis.lampayan@nokia.com>
2021-10-07 16:44:51 +0200

    Subject: The subject Problem: Testing the template Solution: Doing by doing Note: none

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
     dpapi "github.com/intel/intel-device-plugins-for-kubernetes/pkg/deviceplugin"
 )
 
+//adding a comment
 const (
     devicesHostDirectory  = "/dev"
     tpmRmDeviceRegex = `^tpmrm[0-9]*$`
